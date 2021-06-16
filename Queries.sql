5d2cc635148151e2df40ccab8fb79a9165b3a89e
Bernardo Penaherrera <81016335+bernardoprs@users.noreply.github.com>
2021-06-16 16:12:02 -0400

    Add files via upload

---
+++ Queries.sql
@@ -0,0 +1,54 @@
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- There are two conditions in this query, both are in parenthesis and combined using an 'AND'
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- This counts the number of rows in the first_name column
+
+-- Create a table with the list of employees ready to retire
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- 'INTO retirement_info' tells Postgress to save the data into a table named 'retirement_info'
+SELECT * FROM retirement_info;
+
+drop all
+
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+-- 'SELECT COUNT' gives us the total number of employees
+-- 'LEFT JOIN' was used because we wanted all employee numbers from Table 1 to be included
+-- 'GROUP BY' gives us the number of employees retiring from each department
+-- 'ORDER BY' puts the output data in order
+
